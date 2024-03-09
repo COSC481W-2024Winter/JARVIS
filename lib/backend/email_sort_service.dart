db7f4b69e81a8669a2db41342fa3412da974f0b4
emar <49133270+emarron@users.noreply.github.com>
2024-03-09 14:22:03 -0500

    Feature: Implemented modification of model output to be constrained to 4 categories.

--- lib/backend/email_sort_service.dart
+++ lib/backend/email_sort_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:logging/logging.dart';
 
 enum EmailCategory {
   companyBusinessStrategy,
@@ -15,8 +16,17 @@ enum EmailCategory {
 class EmailSorter {
   final String _apiUrl = "https://api-inference.huggingface.co/models/emarron/JARVIS-email-sorter";
   final String _apiToken;
+  final Logger _logger = Logger('EmailSorter');
 
-  EmailSorter({required String apiToken}) : _apiToken = apiToken;
+  EmailSorter({required String apiToken}) : _apiToken = apiToken {
+    _initializeLogger();
+  }
+  void _initializeLogger() {
+    Logger.root.level = Level.ALL; // Set this level as needed
+    Logger.root.onRecord.listen((record) {
+      print('${record.level.name}: ${record.time}: ${record.message}');
+    });
+  }
 
   EmailCategory _mapLabelToCategory(String label) {
     switch (label) {
@@ -45,6 +55,29 @@ class EmailSorter {
     return text.length > maxLength ? text.substring(0, maxLength) : text;
   }
 
+  EmailCategory? getBestCategory(List<dynamic> predictions) {
+    EmailCategory? bestCategory;
+    double highestProbability = 0.0;
+
+    for (var prediction in predictions) {
+      var category = _mapLabelToCategory(prediction['label']);
+      var probability = prediction['score'];
+
+      if (category == EmailCategory.companyBusinessStrategy ||
+          category == EmailCategory.purelyPersonal ||
+          category == EmailCategory.logisticArrangements ||
+          category == EmailCategory.documentEditingCheckingCollaboration) {
+        if (probability > highestProbability) {
+          highestProbability = probability;
+          bestCategory = category;
+        }
+      }
+    }
+
+    return bestCategory;
+  }
+
+
   Future<List<Map<String, dynamic>>> categorizeEmailsList(List<Map<String, String>> emails) async {
     var headers = {
       'Authorization': 'Bearer $_apiToken',
@@ -69,22 +102,30 @@ class EmailSorter {
 
         if (response.statusCode == 200) {
           var jsonResponse = json.decode(response.body);
-          var bestPrediction = jsonResponse[0][0];
-          var categoryLabel = _mapLabelToCategory(bestPrediction['label']).toString().split('.').last;
-          categorizedEmails.add({
-            "Subject": email["Subject"],
-            "Body": email["Body"],
-            "Category": categoryLabel,
-          });
+          var predictions = jsonResponse[0];
+          var bestCategory = getBestCategory(predictions);
+          if (bestCategory != null) {
+            categorizedEmails.add({
+              "Subject": email["Subject"],
+              "Body": email["Body"],
+              "Category": bestCategory.toString().split('.').last,
+            });
+          } else {
+            categorizedEmails.add({
+              "Subject": email["Subject"],
+              "Body": email["Body"],
+              "Category": "No relevant category found",
+            });
+          }
           requestSuccessful = true;
         } else if (response.statusCode == 503) {
           var responseBody = json.decode(response.body);
-          var estimatedWaitTime = responseBody['estimated_time'] ?? 20.0;
-          print('Model loading, waiting for $estimatedWaitTime seconds before retrying...');
+          var estimatedWaitTime = responseBody['estimated_time'] ?? 10.0;
+          _logger.info('Model loading, waiting for $estimatedWaitTime seconds before retrying...');
           await Future.delayed(Duration(seconds: estimatedWaitTime.round()));
         } else {
-          print('Request failed with status: ${response.statusCode}.');
-          print('Response body: ${response.body}');
+          _logger.warning('Request failed with status: ${response.statusCode}.');
+          _logger.warning('Response body: ${response.body}');
           categorizedEmails.add({
             "Subject": email["Subject"],
             "Body": email["Body"],
