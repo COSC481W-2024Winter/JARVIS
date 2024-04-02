f7c2f13f78cb212db720f19cf61d4565e99f4465
emar <49133270+emarron@users.noreply.github.com>
2024-04-02 13:31:33 -0400

    fix: widget test calling

--- test/unit/widget_test.dart
+++ test/unit/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:jarvis/deprecated/main.dart';
+import 'package:jarvis/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
