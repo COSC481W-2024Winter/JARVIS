39ca818b704108b2cb4f9fe21dbb6d684125a3e4
Luna Meng Jia <luna.jia6@gmail.com>
2024-04-09 15:50:08 -0400

    Update weather test

--- test/unit/weather_service_test.dart
+++ test/unit/weather_service_test.dart
@@ -1,13 +1,12 @@
-import 'dart:convert'; // Add this import to use jsonDecode
+import 'dart:convert';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:jarvis/backend/weather_service.dart';
 import 'package:flutter/material.dart';
 
-
 void main() {
   test('WeatherService processWeatherData method processes data correctly', () async {
     // Initialize Widgets binding
-    WidgetsFlutterBinding.ensureInitialized(); // Ensure Flutter engine is initialized
+    WidgetsFlutterBinding.ensureInitialized();
 
     // Example JSON response from the OpenWeatherMap API
     const jsonResponse = '''
@@ -25,14 +24,14 @@ void main() {
     final weatherService = WeatherService();
 
     // Call processWeatherData with the parsed JSON
-    final String result = await weatherService.processWeatherData(jsonMap);
+    final Map<String, String> result = weatherService.processWeatherData(jsonMap);
 
     // Assertions to verify the correctness of the processed data
-    expect(result, contains("Test City"));
-    expect(result, contains("Clouds"));
-    expect(result, contains("10.0°F"));
-    //expect(result, contains("8.0°C"));
-   // expect(result, contains("12.0°C"));
-    expect(result, contains("5.0"));
+    expect(result['condition'], equals('Clouds'));
+    expect(result['temperature'], equals('10.0°F'));
+    expect(result['summary'], contains('Test City'));
+    expect(result['summary'], contains('Clouds'));
+    expect(result['summary'], contains('10.0°F'));
+    expect(result['summary'], contains('5.0'));
   });
 }
